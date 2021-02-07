4ffe576e3906a16379a1cbb86625b086c020e8af
Madhava Carrillo <maduxi@gmail.com>
2021-02-08 00:21:58 +0100

    Cleaned a bit the non complete InfluxDB related files.

--- influxdb/run_influxdb.sh
+++ influxdb/run_influxdb.sh
@@ -1,10 +1,2 @@
 docker pull influxdb
 docker run -d -p 8086:8086 influxdb
-
-
-docker run --rm -e INFLUXDB_HTTP_AUTH_ENABLED=true \
-         -e INFLUXDB_ADMIN_USER=admin \
-         -e INFLUXDB_ADMIN_PASSWORD=admin \
-         -v /var/lib/influxdb:/var/lib/influxdb \
-         -v /etc/influxdb/scripts:/docker-entrypoint-initdb.d \
-         influxdb /init-influxdb.sh
